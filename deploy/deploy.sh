ce1f25151aa19bd9e1b2ac83ccc7627416f7287c
sreekar <sreekar339@gmail.com>
2018-12-27 11:25:49 +0530

    added deploy script

---
+++ deploy/deploy.sh
@@ -0,0 +1,5 @@
+echo "deploying to 139.59.93.218 - sreekar.co"
+ssh sreekar339@139.59.93.218 'bash' < ./deploy/deleteRepo.sh
+
+echo "pushing new version of the build"
+scp -r build sreekar339@139.59.93.218:~/pixi-treasure-hunter-game
