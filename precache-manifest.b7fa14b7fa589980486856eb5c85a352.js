fc10ae60daa6d99581738ad963b7d754dabb95e9
sreekar <sreekar339@gmail.com>
2018-12-27 11:11:55 +0530

    Updates

---
+++ precache-manifest.b7fa14b7fa589980486856eb5c85a352.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5e7b6b07e65b9ca3275",
+    "url": "/PixiTreasureHunter/static/js/runtime~main.b5e7b6b0.js"
+  },
+  {
+    "revision": "3cd5343604cd63ce33c7",
+    "url": "/PixiTreasureHunter/static/js/main.3cd53436.chunk.js"
+  },
+  {
+    "revision": "fc89961d2776d5111711",
+    "url": "/PixiTreasureHunter/static/js/1.fc89961d.chunk.js"
+  },
+  {
+    "revision": "a01da435da249b7555af5c08c4bac8a0",
+    "url": "/PixiTreasureHunter/index.html"
+  }
+];
